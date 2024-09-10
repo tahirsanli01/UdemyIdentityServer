5ec97443ce17e0c85e9dd4fe80cb46e0b4b358e0
ismail Gündoğan <ismailgundogan@gmail.com>
2024-09-10 23:55:26 +0300

    commit all

--- UdemyIdentityServer.Database/Contexts/AuthDbContextPartial.cs
+++ UdemyIdentityServer.Database/Contexts/AuthDbContextPartial.cs
@@ -11,12 +11,12 @@ namespace UdemyIdentityServer.Database.Contexts
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            #if(DEBUG)
-            optionsBuilder.UseSqlServer("Data Source=192.168.2.108;Initial Catalog=AuthDb;Persist Security Info=True;User ID=sa;Password=saw;TrustServerCertificate=True");
-            #else
+            //#if(Debug)
+            //optionsBuilder.UseSqlServer("Data Source=192.168.2.108;Initial Catalog=AuthDb;Persist Security Info=True;User ID=sa;Password=saw;TrustServerCertificate=True");
+            //#else
+            //optionsBuilder.UseSqlServer("Persist Security Info=False;Initial Catalog=AuthDb;Data Source=176.235.236.6; User ID=sa; Password=2656_Tahir;TrustServerCertificate=True;Encrypt=True");
+            //#endif
             optionsBuilder.UseSqlServer("Persist Security Info=False;Initial Catalog=AuthDb;Data Source=176.235.236.6; User ID=sa; Password=2656_Tahir;TrustServerCertificate=True;Encrypt=True");
-            #endif
-
             base.OnConfiguring(optionsBuilder);
         }
     }
